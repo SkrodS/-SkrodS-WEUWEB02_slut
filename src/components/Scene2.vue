cb3e2eb69cccdf7b619fd571430792c48a33bc15
SkrodS <r.widjeback@hotmail.se>
2022-05-07 13:01:09 +0200

    scroll behavior

--- src/components/Scene2.vue
+++ src/components/Scene2.vue
@@ -3,20 +3,27 @@ import { gsap } from "gsap";
 
 export default {
     mounted() {
+        var tl2 = gsap.timeline({onReverseComplete: true});
+        tl2.from('#card1', {delay: 3, scale: .2, opacity: 0, duration: .4, ease: 'power2.inOut'})
+        tl2.from('#card2', {scale: .2, opacity: 0, duration: .4, ease: 'power2.inOut'})
+        tl2.from('#card3', {scale: .2, opacity: 0, duration: .4, ease: 'power2.inOut'})
+        tl2.from('#card4', {scale: .2, opacity: 0, duration: .4, ease: 'power2.inOut'})
+        tl2.from('#scroll-text', {scale: .2, opacity: 0, duration: .4, ease: 'power2.inOut'})
     },
 }
 </script>
 
 <template>
   <div ref="container" id="container" class="relative h-screen w-full flex justify-center content-center overflow-hidden dark:bg-gray-900 bg-slate-200 color-anim">
-    <VueWriter :array="['Projects']" :start="5300" :delay="1500" :typeSpeed="100" :iterations="1" :eraseSpeed="50" class="absolute mx-auto z-10 mt-16 text-6xl font-metana font-bold tracking-tighter text-orange-400 text-shadow-sm opacity-90" />
+    <VueWriter :array="['Projects']" :start="5300" :typeSpeed="100" :iterations="1" :eraseSpeed="50" class="absolute mx-auto z-10 mt-16 text-6xl font-metana font-bold tracking-tighter text-orange-400 text-shadow-sm opacity-90" />
+    <p id="scroll-text" class="absolute mx-auto mt-32 text-xl font-newake font-bold tracking-widest text-white text-shadow-sm opacity-90">(Scroll <i class="fa-solid fa-arrow-right-long"></i>)</p>
     <div class="flex items-center justify-center">
       <div class="w-full pattern-wavy pattern-gray-900 pattern-bg-white pattern-size-32 pattern-opacity-5 absolute color-anim"></div>
 
-        <div class="grid lg:grid-cols-4 ml-[5rem] p-16 gap-6 grid-cols-1">
+        <div class="flex flex-no-wrap overflow-x-scroll scrolling-touch items-start mt-12 ml-[5rem] no-scrollbar">
           
           <!-- CARD -->
-          <div class="relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
+          <div id="card1" class="ml-8 flex-none w-1/3 md:w-1/3 mr-8 md:pb-4 relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
             <a href="src/assets/Screenshot from 2022-05-06 11-20-10.png" target="_blank">
               <img class="h-80 w-full object-cover hover:blur-sm transition-all" src="src/assets/Screenshot from 2022-05-06 11-20-10.png" alt="" />
             </a>
@@ -36,7 +43,7 @@ export default {
           </div>
 
           <!-- CARD -->
-          <div class="relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
+          <div id="card2" class="flex-none w-1/3 md:w-1/3 mr-8 md:pb-4 relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
             <a href="src/assets/smartmockups_klawjpjg.jpg" target="_blank">
               <img class="h-80 w-full object-cover hover:blur-sm transition-all" src="src/assets/smartmockups_klawjpjg.jpg" alt="" />
             </a>
@@ -56,7 +63,7 @@ export default {
           </div>
 
           <!-- CARD -->
-          <div class="relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
+          <div id="card3" class="flex-none w-1/3 md:w-1/3 mr-8 md:pb-4 relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
             <a href="src\assets\Skärmbild 2022-05-06 200821.png" target="_blank">
               <img class="h-80 w-full object-cover hover:blur-sm transition-all" src="src\assets\Skärmbild 2022-05-06 200821.png" alt="" />
             </a>
@@ -76,19 +83,19 @@ export default {
           </div>
 
           <!-- CARD -->
-          <div class="relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
-            <a href="src/assets/smartmockups_klawjpjg.jpg" target="_blank">
-              <img class="h-80 w-full object-cover hover:blur-sm transition-all" src="src/assets/smartmockups_klawjpjg.jpg" alt="" />
+          <div id="card4" class="flex-none w-1/3 md:w-1/3 mr-8 md:pb-4 relative max-w-xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-xl z-50 bg-opacity-60 border border-gray-200 rounded-xl color-anim">
+            <a href="src/assets/Screenshot from 2022-05-06 11-39-51.png" target="_blank">
+              <img class="h-80 w-full object-cover hover:blur-sm transition-all" src="src/assets/Screenshot from 2022-05-06 11-39-51.png" alt="" />
             </a>
             <div class="p-5 mb-12">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white color-anim font-metana">Share:y</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-white color-anim font-metana">EC Store</h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-400 color-anim">This is a project I worked on for school. It's social media platform called Share:y with fully functional back-end + front-end, made by me.</p>
-                <p class="mb-3 font-normal text-gray-400 color-anim">The back-end enables the users to create accounts, make posts, visit other peoples profiles.</p>
+                <p class="mb-3 font-normal text-gray-400 color-anim">For this project I created a whole webshop including back-end and front-end.</p>
+                <p class="mb-3 font-normal text-gray-400 color-anim">The website works as you'd imagine, you can put items in your shopping cart, create orders and it even has an admin page where everything can be managed by an owner.</p>
 
                 <div class="absolute bottom-0 h-16 inset-x-0">
-                  <a href="https://github.com/SkrodS/bizard-game" class="inline-flex items-center py-2 px-3 w-full text-2xl font-medium text-center text-black bg-orange-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-orange-400 dark:hover:bg-blue-800 dark:focus:ring-blue-800 color-anim">
+                  <a href="https://github.com/SkrodS/ec-store" class="inline-flex items-center py-2 px-3 w-full text-2xl font-medium text-center text-black bg-orange-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-orange-400 dark:hover:bg-blue-800 dark:focus:ring-blue-800 color-anim">
                     <p class="mx-auto font-newake tracking-wider">Visit repo: <i class="fa-brands fa-github"></i></p>
                   </a>
               </div>
@@ -103,4 +110,14 @@ export default {
 </template>
 
 <style scoped>
+/* Hide scrollbar for Chrome, Safari and Opera */
+.no-scrollbar::-webkit-scrollbar {
+    display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.no-scrollbar {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
 </style>
